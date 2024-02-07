4c4fb6b05df07f7b1a034123401922a363cbb22d
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2024-02-07 16:53:47 +0100

    Initial commit

---
+++ src/test/java/com/epicode/U5D2/MyTest.java
@@ -0,0 +1,14 @@
+package com.epicode.U5D2;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    public void checkPizzaBasicTopping(){
+    }
+
+    @Test
+    public void checkTableStatus(){
+    }
+}
